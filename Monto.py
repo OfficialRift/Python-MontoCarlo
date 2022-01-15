fc707db6273816a195765b002bed7a34bbc2d4e4
Sebastian Stenbom <ryezze@gmail.com>
2022-01-15 22:17:12 +0100

    add code

---
+++ Monto.py
@@ -0,0 +1,16 @@
+import random
+  
+inside_points= 0
+outside_points= 0
+  
+for i in range(1000000):
+    x= random.uniform(-1, 1) # between 0 and 1
+    y= random.uniform(-1, 1)
+  
+    if x**2 + y**2<= 1:
+        inside_points+= 1
+  
+    outside_points+= 1
+  
+    pi = 4* inside_points/ outside_points
+    print("Pi= ", pi)
